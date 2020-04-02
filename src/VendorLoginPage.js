804aeaf540836e667d3bf85eb2d4318c3f7aa34a
Anh Trinh <trinhhaianh37@gmail.com>
2020-04-03 09:11:47 +1100

    init page

--- src/VendorLoginPage.js
+++ src/VendorLoginPage.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './VendorLoginPage.css';
 
 function VendorLoginPage() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/VendorLoginPage.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div>
+            HELLO WORLD!!!
         </div>
     );
 }
