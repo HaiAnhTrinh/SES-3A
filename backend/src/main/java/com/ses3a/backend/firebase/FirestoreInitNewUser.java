50a62368b8c843d05488a1c979f0bc35905a7251
Anh Trinh <trinhhaianh37@gmail.com>
2020-04-29 09:40:58 +1000

    finish init new user

--- backend/src/main/java/com/ses3a/backend/firebase/FirestoreInitNewUser.java
+++ backend/src/main/java/com/ses3a/backend/firebase/FirestoreInitNewUser.java
@@ -1,13 +1,34 @@
 package com.ses3a.backend.firebase;
 
 import com.google.cloud.firestore.Firestore;
+import com.ses3a.backend.entity.request.CreateNewUserRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FirestoreInitNewUser {
-    protected static void initVendor(Firestore firestore, String email){
 
+    //TODO: if the final product does not require difference, merge them into 1
+
+    protected static void initVendor(Firestore firestore, CreateNewUserRequest request){
+        System.out.println("INIT VENDORS CALLED");
+        Map<String, Object> emptyData = new HashMap<>();
+        firestore.collection("users")
+                .document(FirebaseUtils.convertToUserType(request.getRole()))
+                .collection(request.getEmail())
+                .document("products")
+                .collection("inStore")
+                .document("nothing").set(emptyData);
     }
 
-    protected static void initSupplier(Firestore firestore, String email){
-
+    protected static void initSupplier(Firestore firestore, CreateNewUserRequest request){
+        System.out.println("INIT SUPPLIERS CALLED");
+        Map<String, Object> emptyData = new HashMap<>();
+        firestore.collection("users")
+                .document(FirebaseUtils.convertToUserType(request.getRole()))
+                .collection(request.getEmail())
+                .document("products")
+                .collection("inStore")
+                .document("nothing").set(emptyData);
     }
 }
