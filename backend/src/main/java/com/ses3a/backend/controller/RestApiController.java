436596277bb118354e5e0022263ba091a9c41006
Anh Trinh <trinhhaianh37@gmail.com>
2020-04-29 09:39:49 +1000

    Finish GetProductByCategory

--- backend/src/main/java/com/ses3a/backend/controller/RestApiController.java
+++ backend/src/main/java/com/ses3a/backend/controller/RestApiController.java
@@ -1,22 +1,25 @@
 package com.ses3a.backend.controller;
 
+import com.ses3a.backend.entity.object.SupplierProduct;
 import com.ses3a.backend.entity.request.CreateNewUserRequest;
 import com.ses3a.backend.entity.request.EditUserInfoRequest;
+import com.ses3a.backend.entity.request.GetProductByCategoryRequest;
 import com.ses3a.backend.entity.request.GetUserInfoRequest;
 import com.ses3a.backend.entity.response.CreateNewUserResponse;
 import com.ses3a.backend.entity.response.EditUserInfoResponse;
+import com.ses3a.backend.entity.response.GetProductByCategoryResponse;
 import com.ses3a.backend.entity.response.GetUserInfoResponse;
 import com.ses3a.backend.firebase.FirebaseServices;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 @RestController
 public class RestApiController {
     final FirebaseServices firebaseServices;
+//    private Firestore firestore = FirestoreClient.getFirestore();
 
     public RestApiController(FirebaseServices firebaseServices) {
         this.firebaseServices = firebaseServices;
@@ -37,7 +40,8 @@ public class RestApiController {
     /*******************************************prototype functions****************************************************/
     @CrossOrigin(origins = "*")
     @GetMapping("/GetUserInfo")
-    public ResponseEntity<GetUserInfoResponse> getUserInfo(@RequestHeader String email, @RequestHeader String role){
+    public ResponseEntity<GetUserInfoResponse>
+    getUserInfo(@RequestHeader String email, @RequestHeader String role){
         System.out.println("RECEIVED GET USER INFO REQUEST");
         GetUserInfoRequest request = new GetUserInfoRequest(email, role);
         ResponseEntity<GetUserInfoResponse> responseEntity =
@@ -55,13 +59,33 @@ public class RestApiController {
 
     @CrossOrigin(origins = "*")
     @PostMapping("/EditUserInfo")
-    public ResponseEntity<EditUserInfoResponse> editUserInfo(@RequestBody EditUserInfoRequest editUserInfoRequest){
+    public ResponseEntity<EditUserInfoResponse>
+    editUserInfo(@RequestBody EditUserInfoRequest editUserInfoRequest){
         System.out.println("RECEIVED EDIT USER INFO REQUEST");
         ResponseEntity<EditUserInfoResponse> responseEntity =
                 new ResponseEntity<>(new EditUserInfoResponse(), HttpStatus.OK);
         firebaseServices.editUserInfo(editUserInfoRequest);
         Objects.requireNonNull(responseEntity.getBody()).setStatus("Success");
         responseEntity.getBody().setMessage("User info has been edited");
+        return responseEntity;
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/GetProductByCategory")
+    public ResponseEntity<GetProductByCategoryResponse>
+    getProductByCategory(@RequestHeader String email, @RequestHeader String category){
+        System.out.println("RECEIVED GET PRODUCT BY CATEGORY REQUEST");
+        GetProductByCategoryRequest request = new GetProductByCategoryRequest(email, category);
+        ResponseEntity<GetProductByCategoryResponse> responseEntity =
+                new ResponseEntity<>(new GetProductByCategoryResponse(), HttpStatus.OK);
+        try{
+            List<SupplierProduct> products = firebaseServices.getProductByCategory(request);
+            Objects.requireNonNull(responseEntity.getBody()).setStatus("Success");
+            responseEntity.getBody().setProducts(products);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
         return responseEntity;
     }
 }
--- backend/src/main/java/com/ses3a/backend/firebase/FirebaseServices.java
+++ backend/src/main/java/com/ses3a/backend/firebase/FirebaseServices.java
@@ -1,27 +1,24 @@
 package com.ses3a.backend.firebase;
 
+import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.SetOptions;
 import com.google.firebase.cloud.FirestoreClient;
+import com.ses3a.backend.entity.object.SupplierProduct;
 import com.ses3a.backend.entity.request.CreateNewUserRequest;
 import com.ses3a.backend.entity.request.EditUserInfoRequest;
+import com.ses3a.backend.entity.request.GetProductByCategoryRequest;
 import com.ses3a.backend.entity.request.GetUserInfoRequest;
 import org.springframework.stereotype.Service;
 import javax.validation.constraints.NotNull;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
+
+import static com.ses3a.backend.firebase.FirebaseUtils.convertToUserType;
 
 @Service
 public class FirebaseServices {
-
-    //convert roles on the app into dtb user type
-    private String convertToUserType(String role){
-        if(role.equals("Business owner")){
-            return "vendors";
-        }
-        return "suppliers";
-    }
 
     //Add newly registered user to Firestore
     public void createNewUser(@NotNull CreateNewUserRequest request){
@@ -32,10 +29,10 @@ public class FirebaseServices {
         String userType = convertToUserType(request.getRole());
 
         if(userType.equals("vendors")){
-            FirestoreInitNewUser.initVendor(firestore, request.getEmail());
+            FirestoreInitNewUser.initVendor(firestore, request);
         }
         else{
-            FirestoreInitNewUser.initSupplier(firestore, request.getEmail());
+            FirestoreInitNewUser.initSupplier(firestore, request);
         }
 
         firestore.collection("users")
@@ -44,7 +41,6 @@ public class FirebaseServices {
                 .document("userInfo")
                 .set(data);
     }
-
 
     /*******************************************prototype functions****************************************************/
     //Get userInfo from Firestore
@@ -72,5 +68,36 @@ public class FirebaseServices {
                 .collection(request.getEmail())
                 .document("userInfo")
                 .set(data, SetOptions.merge());
+    }
+
+    //Get products by category in Firestore
+    public List<SupplierProduct> getProductByCategory(GetProductByCategoryRequest request)
+            throws ExecutionException, InterruptedException {
+        Firestore firestore = FirestoreClient.getFirestore();
+        Iterable<CollectionReference> collectionRefs =
+                firestore.collection("products")
+                        .document(request.getCategory())
+                        .listCollections();
+        List<SupplierProduct> products = new ArrayList<>();
+
+        for(CollectionReference ref : collectionRefs){
+            List<QueryDocumentSnapshot> documents =
+                    ref.get().get().getDocuments();
+
+            for(QueryDocumentSnapshot document : documents){
+                SupplierProduct product = new SupplierProduct();
+                product.setSupplierEmail(ref.getId());
+                product.setProductName(document.getId());
+                product.setProductPrice(document.getString("price"));
+                product.setProductQuantity(document.getString("quantity"));
+                products.add(product);
+            }
+        }
+        return products;
+    }
+
+    //Add and edit products in Firestore
+    public void addAndEditProducts(){
+
     }
 }
