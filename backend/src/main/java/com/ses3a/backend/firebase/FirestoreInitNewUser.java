f6b6acae9a7f97266d908f28eebf8df62b1e16b5
Anh Trinh <trinhhaianh37@gmail.com>
2020-04-29 15:15:19 +1000

    modify create new user

--- backend/src/main/java/com/ses3a/backend/firebase/FirestoreInitNewUser.java
+++ backend/src/main/java/com/ses3a/backend/firebase/FirestoreInitNewUser.java
@@ -13,22 +13,22 @@ public class FirestoreInitNewUser {
     protected static void initVendor(Firestore firestore, CreateNewUserRequest request){
         System.out.println("INIT VENDORS CALLED");
         Map<String, Object> emptyData = new HashMap<>();
+        emptyData.put("empty", true);
         firestore.collection("users")
                 .document(FirebaseUtils.convertToUserType(request.getRole()))
                 .collection(request.getEmail())
                 .document("products")
-                .collection("inStore")
-                .document("nothing").set(emptyData);
+                .create(emptyData);
     }
 
     protected static void initSupplier(Firestore firestore, CreateNewUserRequest request){
         System.out.println("INIT SUPPLIERS CALLED");
         Map<String, Object> emptyData = new HashMap<>();
+        emptyData.put("empty", true);
         firestore.collection("users")
                 .document(FirebaseUtils.convertToUserType(request.getRole()))
                 .collection(request.getEmail())
                 .document("products")
-                .collection("inStore")
-                .document("nothing").set(emptyData);
+                .create(emptyData);
     }
 }
